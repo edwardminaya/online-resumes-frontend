f3a7c0d55b2683c8bca0b4d8b97d4c711cc7b95e
NeilEllison <ellison.neil1@gmail.com>
2023-05-02 12:15:02 -0700

    skills input with hardcoded info

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,15 +1,17 @@
 import { ResumesIndex } from "./ResumesIndex";
+import { Skills } from "./Skills";
 
 export function Content() {
   const students = [
     { id: 1, first_name: "First", last_name: "Last", photo: 150, bio: 150 },
     { id: 2, first_name: "First", last_name: "Last", photo: 150, bio: 150 },
-    { id: 3, first_name: "First", last_name: "Last", photo: 150, bio: 150 },
+    { id: 3, first_name: "First", last_name: "Last", photo: 150, bio: 150 }
   ];
-
+ 
   return (
     <div>
       <ResumesIndex students={students} />
+      <Skills />
     </div>
   );
 }
