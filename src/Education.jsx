6dd027aa06b3cdf1b9c42f2148bdb079e767feff
mikemccomb <mikemccomb@gmail.com>
2023-05-02 13:35:56 -0500

    Creates Education framework

---
+++ src/Education.jsx
@@ -0,0 +1,11 @@
+export function Education() {
+  return (
+    <div>
+      <h1>Education</h1>
+      <h2>Grad School</h2>
+      <h2>Undergrad</h2>
+      <h2>High School</h2>
+      <h2>Certificates</h2>
+    </div>
+  );
+}
