4bced0374bb25b26421d87fc985074f959c85dbe
NeilEllison <ellison.neil1@gmail.com>
2023-05-02 10:44:49 -0700

    frontend setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-// Hello from Ed
-// Hello from Neil
-// Yooooooo -Anna
-// It's crazy. It's party.
-function App() {
-  const [count, setCount] = useState(0);
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { Footer } from "./Footer";
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   );
 }
 
