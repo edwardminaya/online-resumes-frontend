113fc10184db3c7e3e95ced0faf010413ca305af
mikemccomb <mikemccomb@gmail.com>
2023-05-02 14:20:34 -0500

    Roughs out capstone component

--- src/Capstone.jsx
+++ src/Capstone.jsx
@@ -1,7 +1,18 @@
-export function Capstone() {
+export function Capstone(props) {
   return (
     <div>
-      <p>Capstone project</p>
+      <h2>Capstone Projects</h2>
+      {props.capstones.map((capstone) => (
+        <div key={capstone.id}>
+          <h3>
+            <a href={capstone.url} target="_blank" rel="noreferrer">
+              {capstone.project_name}
+            </a>
+            <img src={capstone.screenshot} width="300px" />
+          </h3>
+          <p>{capstone.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/Content.jsx
+++ src/Content.jsx
@@ -8,10 +8,27 @@ export function Content() {
     { id: 3, first_name: "First", last_name: "Last", photo: 150, bio: 150 },
   ];
 
+  const capstones = [
+    {
+      id: 1,
+      project_name: "Weather App",
+      description: "Did you see the movie Twister? That.",
+      url: "http://mistertwister.co",
+      screenshot: "https://cdn.pixabay.com/photo/2014/04/03/10/26/tornado-310431_960_720.png",
+    },
+    {
+      id: 2,
+      project_name: "Birdle",
+      description: "Guess the bird in 6 chirps",
+      url: "http://birdle-game.org",
+      screenshot: "https://cdn.pixabay.com/photo/2016/05/26/14/39/parrot-1417286_960_720.png",
+    },
+  ];
+
   return (
     <div>
       <ResumesIndex students={students} />
-      <Capstone />
+      <Capstone capstones={capstones} />
     </div>
   );
 }
