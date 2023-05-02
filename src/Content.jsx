f924ffbf465c1bd85123d642d9926ccac8d7cb57
NeilEllison <ellison.neil1@gmail.com>
2023-05-02 11:05:09 -0700

    index action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,15 @@
+import { ResumesIndex } from "./ResumesIndex";
+
 export function Content() {
+  const students = [
+    { id: 1, first_name: "First", last_name: "Last", photo: 150, bio: 150 },
+    { id: 2, first_name: "First", last_name: "Last", photo: 150, bio: 150 },
+    { id: 3, first_name: "First", last_name: "Last", photo: 150, bio: 150 },
+  ];
+
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <ResumesIndex students={students} />
     </div>
   );
 }
