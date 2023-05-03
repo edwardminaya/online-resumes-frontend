99a0bef30f9566ad991f32c3f1554a724d090645
mikemccomb <mikemccomb@gmail.com>
2023-05-03 13:57:50 -0500

    Adds styling to the page

--- src/ExperiencesIndex.jsx
+++ src/ExperiencesIndex.jsx
@@ -1,15 +1,19 @@
 export function ExperiencesIndex(props) {
   return (
     <div>
-      <h1>Experience</h1>
+      <h2>Experience</h2>
       {props.experiences.map((experience) => (
-        <div key={experience.id}>
-          <p>Title: {experience.job_title}</p>
-          <p>Company: {experience.company_name}</p>
-          <p>
-            Start Date: {experience.start_date} - End Date: {experience.end_date}
-          </p>
-          <p>{experience.details}</p>
+        <div key={experience.id} className="card">
+          <ul className="list-group list-group-flush">
+            <li>
+              <h3>{experience.job_title}</h3>
+            </li>
+            <li className="list-group-item">Company: {experience.company_name}</li>
+            <li className="list-group-item">
+              Start Date: {experience.start_date} - End Date: {experience.end_date}
+            </li>
+            <li className="list-group-item">{experience.details}</li>
+          </ul>
         </div>
       ))}
     </div>
