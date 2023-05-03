b9295bf19272ee931ef3c4e1dee80d29e084179b
mikemccomb <mikemccomb@gmail.com>
2023-05-03 11:33:56 -0500

    Updates names for education(s) data; adds education section to Content for now

--- src/Education.jsx
+++ src/Education.jsx
@@ -2,13 +2,13 @@ export function Education(props) {
   return (
     <div>
       <h2>Education</h2>
-      {props.schools.map((school) => (
-        <div key={school.id}>
-          <h3>{school.school_name}</h3>
+      {props.educations.map((education) => (
+        <div key={education.id}>
+          <h3>{education.university_name}</h3>
           <p>
-            Attended: {school.start_date} — {school.end_date}
+            Attended: {education.start_date} — {education.end_date}
           </p>
-          <p>Description: {school.details}</p>
+          <p>Description: {education.details}</p>
         </div>
       ))}
     </div>
