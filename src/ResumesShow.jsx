22f3ed2f3ead3c825ab785ebfb65e7e8ae0bd91f
“Edward <eminaya49@gmail.com>
2023-05-04 15:36:01 -0500

    updated experience and added scroll

--- src/ResumesShow.jsx
+++ src/ResumesShow.jsx
@@ -2,16 +2,33 @@
 export function ResumesShow(props) {
   console.log(props);
   return (
-    <div>
+    <div id="modal">
       <h1>
         {props.resume.first_name} {props.resume.last_name}
       </h1>
 
-      {props.resume.experiences.map((experience) => (
-        <div key={experience.id}>
-          <p>{experience.job_title}</p>
-        </div>
-      ))}
+      <div className="experiences">
+        <h2>Experience</h2>
+        {props.resume.experiences.map((experience) => (
+          <div className="container" key={experience.id}>
+            <div className="row">
+              <div className="col">
+                <p>Job Title: {experience.job_title}</p>
+              </div>
+              <div id="dates" className="col">
+                <p>
+                  {experience.start_date} - {experience.end_date}
+                </p>
+              </div>
+              <p id="company">{experience.company_name}</p>
+              <ul id="list">
+                <li>{experience.details}</li>
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+
       {props.resume.capstones.map((capstone) => (
         <div key={capstone.id}>
           <p>{capstone.name}</p>
