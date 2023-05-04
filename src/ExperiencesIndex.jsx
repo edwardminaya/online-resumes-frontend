7d96f60528cc77970b7b67cb093f65c32402228d
“Edward <eminaya49@gmail.com>
2023-05-04 13:53:55 -0500

    updated experience index

--- src/ExperiencesIndex.jsx
+++ src/ExperiencesIndex.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
 export function ExperiencesIndex(props) {
   return (
     <div>
       <h2>Experience</h2>
       {props.students.map((student) => (
         <div key={student.id} className="card">
-          <p>{student.first_name}</p>
+          {student.experiences.map((experience) => (
+            <div>
+              <h3>{experience.job_title}</h3>
+              <h3>{experience.company_name}</h3>
+              <h4>
+                {experience.start_date} - {experience.end_date}
+              </h4>
+              <p>{experience.details}</p>
+            </div>
+          ))}
         </div>
       ))}
     </div>
