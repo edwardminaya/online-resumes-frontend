b293a6586028d82e40e4d8006469aae2a81102fd
Anna <annamtoman@gmail.com>
2023-05-04 15:15:32 -0500

    adds data

--- src/ResumesShow.jsx
+++ src/ResumesShow.jsx
@@ -6,6 +6,10 @@ export function ResumesShow(props) {
       <h1>
         {props.resume.first_name} {props.resume.last_name}
       </h1>
+        <img src={props.resume.image_url} />
+        <h3>{props.resume.email}</h3>
+        <h3>{props.resume.phone_number}</h3>
+        <p>{props.resume.short_bio}</p>
 
       {props.resume.experiences.map((experience) => (
         <div key={experience.id}>
